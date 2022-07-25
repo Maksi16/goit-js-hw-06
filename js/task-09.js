554a7952d59923ada935b01f0bcd953da9e0b782
Maksi16 <maksim.moroz.kr@gmail.com>
2022-07-26 00:21:14 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+changeColorBtn.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
 }
