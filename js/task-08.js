ab57881e82a26e1a32c389b93cfcbbc47626efe2
Maksi16 <maksim.moroz.kr@gmail.com>
2022-07-26 00:47:17 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handBtnSubmit);
+
+function handBtnSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  const formElements = event.currentTarget.elements;
+
+  const userMail = formElements.email.value;
+  const userPassword = formElements.password.value;
+  const formData = {
+    userMail,
+    userPassword,
+  };
+  console.log(formData);
+  event.target.reset();
+}
